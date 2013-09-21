bf5db05c03730f7c5c3ef48fa91ca729cc7478f1
Velesey <vladimir9292@list.ru>
2013-09-21 21:40:24 +0600

    Update blocks.py
    Глюк с картинками, когда запускали из консоли

--- blocks.py
+++ blocks.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from pygame import *
+import os
 
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
-
+ICON_DIR = os.path.dirname(__file__) #  Полный путь к каталогу с файлами
  
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         self.image.fill(Color(PLATFORM_COLOR))
-        self.image = image.load("blocks/platform.png")
+        self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
-
